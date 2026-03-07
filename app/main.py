05eefc6ed1366b5b129b2f5c3aa739133cb1720a
sammy <lednevdosa@gmail.com>
2026-03-07 10:44:12 +0300

    main bot

---
+++ app/main.py
@@ -0,0 +1,25 @@
+import asyncio
+import logging
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config.settings import settings
+from aiogram import Bot, Dispatcher
+
+bot = Bot(
+    token=settings.bot.TOKEN.get_secret_value(),
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+)
+
+dp = Dispatcher()
+
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
